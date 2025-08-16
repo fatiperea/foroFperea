45864768260740fdb524545564cd07e0f2feb39d
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-16 17:32:41 -0300

    primer commit

---
+++ src/main/java/com/perea/ForoFPerea/ForoFPereaApplication.java
@@ -0,0 +1,13 @@
+package com.perea.ForoFPerea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoFPereaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoFPereaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perea/ForoFPerea/ForoFPereaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perea.ForoFPerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoFPereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
