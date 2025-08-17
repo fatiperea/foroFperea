b29f9795affddc8c0be1446588c51564718ff510
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-17 10:45:26 -0300

    creo el end point y el dto para datos lista topico

--- src/main/java/com/perea/ForoFPerea/controller/TopicoController.java
+++ src/main/java/com/perea/ForoFPerea/controller/TopicoController.java
@@ -1,15 +1,15 @@
 package com.perea.ForoFPerea.controller;
 
+import com.perea.ForoFPerea.topico.DatosListaTopico;
 import com.perea.ForoFPerea.topico.DatosRegistroTopico;
 import com.perea.ForoFPerea.topico.Topico;
 import com.perea.ForoFPerea.topico.TopicoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -25,6 +25,11 @@ public class TopicoController {
         //System.out.println(datos);
         topicoRepository.save(new Topico(datos));
 
+    }
+
+    @GetMapping
+    public List<DatosListaTopico> listar(){
+        return topicoRepository.findAll();
     }
     /*public ResponseEntity registrar(@RequestBody @Valid DatosRegistroMedico datos, UriComponentsBuilder uriComponentsBuilder){
         //System.out.println(datos);
---
+++ src/main/java/com/perea/ForoFPerea/topico/DatosListaTopico.java
@@ -0,0 +1,19 @@
+package com.perea.ForoFPerea.topico;
+
+import com.perea.ForoFPerea.autor.Autor;
+import com.perea.ForoFPerea.curso.Curso;
+
+import java.time.LocalDateTime;
+
+public record DatosListaTopico(
+
+        String titulo,
+        String mensaje,
+        LocalDateTime fechaCreación,
+        StatusTopico status,
+        Autor autor,
+        Curso curso
+) {
+
+
+}
