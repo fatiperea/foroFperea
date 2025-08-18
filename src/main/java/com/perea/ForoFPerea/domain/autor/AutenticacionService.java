5114a0c97ec4c5ee9e1ee5a8614b26620cfeac25
fatiperea <fatimaperea3pppp0@gmail.com>
2025-08-18 19:34:48 -0300

    esperaba utilizar mi clase autor para la autenticacion pero como es bastabte mas complejo conservar la definicion de esa clase y esa tabla autor y tratar de continuar el intento de autenticacion, se parasa a crear una clase usuario directamente

--- src/main/java/com/perea/ForoFPerea/domain/autor/AutenticacionService.java
+++ src/main/java/com/perea/ForoFPerea/domain/autor/AutenticacionService.java
@@ -2,18 +2,19 @@ package com.perea.ForoFPerea.domain.autor;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AutenticacionService {
+public class AutenticacionService implements UserDetailsService {
 
     @Autowired
     private AutorRepository repository;
 
-    //@Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return repository.findByEmail(username);
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return repository.findByEmail(email);
 
     }
 }
